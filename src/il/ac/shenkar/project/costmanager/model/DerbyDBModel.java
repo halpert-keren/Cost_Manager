7f17c381d2b50b9c73867288243d29b6f7d3a352
Danityec <Danityec@gmail.com>
2021-03-03 17:45:25 +0200

    update files derbyDB and IModel

--- src/il/ac/shenkar/project/costmanager/model/DerbyDBModel.java
+++ src/il/ac/shenkar/project/costmanager/model/DerbyDBModel.java
@@ -1,14 +1,68 @@
+/**
+ * The DerbyDB class is used to connect and to correspond with the actual DB.
+ * This class is implemented using a the syntax of Derby specific language.
+ *
+ * @param connection is an object of type Connection to be used to establish the connection the the DB
+ * @param statement is an object of type Statement to be used for executing requests to the DB
+ * @param rs is an object of type ResultSet ti be used for the data returned by the DB from a query
+ */
+
 package il.ac.shenkar.project.costmanager.model;
 
-public class DerbyDBModel implements IModel{
+import java.sql.*;
+
+public class DerbyDBModel implements IModel {
+    private static String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+    private static String protocol = "jdbc:derby:";
+
+
     @Override
     public void addCostItem(CostItem item) throws CostManagerException {
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            Class.forName(driver);
+
+            connection = DriverManager.getConnection(protocol);
+            statement = connection.createStatement();
+
+            statement.executeUpdate("INSERT INTO " + item.getCategoryName() + " (DESCRIPTION, PRICE, CURRENCY, DATE) VALUES('" + item.getDescription() + "','" + item.getSum() + "','" + item.getCurrency() + "','" + item.getDate() + "')");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new CostManagerException(e.getMessage(), e.getCause());
+
+        } finally {
+            if (statement != null)
+                try {
+                    statement.close();
+                } catch (Exception e) {
+                    throw new CostManagerException(e.getMessage(), e.getCause());
+
+                }
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (Exception e) {
+                    throw new CostManagerException(e.getMessage(), e.getCause());
+
+                }
+        }
+    }
+
+    @Override
+    public void addCategory(Category category) throws CostManagerException {
 
     }
 
     @Override
     public CostItem[] getCostItems() throws CostManagerException {
         return new CostItem[0];
+    }
+
+    @Override
+    public Category[] getCategories() throws CostManagerException {
+        return new Category[0];
     }
 
     @Override
--- src/il/ac/shenkar/project/costmanager/model/IModel.java
+++ src/il/ac/shenkar/project/costmanager/model/IModel.java
@@ -1,8 +1,17 @@
 package il.ac.shenkar.project.costmanager.model;
 
 public interface IModel {
+
     public void addCostItem(CostItem item) throws CostManagerException;
+    public void addCategory(Category category) throws CostManagerException;
     public CostItem[] getCostItems() throws CostManagerException;
+    public Category[] getCategories() throws CostManagerException;
     public void deleteCostItem(CostItem item) throws CostManagerException;
+
+
+
+//    public void createDB() throws CostManagerException;
+//    public void closeConnection() throws CostManagerException;
+
 
 }
