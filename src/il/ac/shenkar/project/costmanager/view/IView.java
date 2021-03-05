bc3a160487d33f427fffc7100b3741cdb98d1d3b
halpert-keren <halpertkeren@gmail.com>
2021-03-05 15:22:05 +0200

    IView code cleanup, optimization and comments

--- src/il/ac/shenkar/project/costmanager/view/IView.java
+++ src/il/ac/shenkar/project/costmanager/view/IView.java
@@ -4,13 +4,49 @@ import il.ac.shenkar.project.costmanager.model.Category;
 import il.ac.shenkar.project.costmanager.model.CostItem;
 import il.ac.shenkar.project.costmanager.viewmodel.IViewModel;
 
-import java.util.*;
+import java.util.Map;
 
+/**
+ * The IView interface represents the view portion of the MVVM architecture structure of the Cost Manager.
+ * The IModel interface needs to be implemented by a concrete class
+ * that will be used as the direct access and connection to the user in a user interface.
+ */
 public interface IView {
-    public void setViewModel(IViewModel vm);
-    public void displayPieChart(Map map);
-    public void showMessage(String text);
-    public void showItems(CostItem[] items, String type);
-    public void showCategories(Category[] categories);
 
+    /**
+     * The method to set the ViewModel object so that the View has the connection needed to function.
+     *
+     * @param vm the ViewModel object that is a concrete object of the IViewModel type.
+     */
+    void setViewModel(IViewModel vm);
+
+    /**
+     * The method to display the pie chart of the cost items in the user interface.
+     *
+     * @param map the map representation of every category and its corresponding sum of expenses in it.
+     */
+    void displayPieChart(Map map);
+
+    /**
+     * The method to display a message in the user interface.
+     *
+     * @param text the string of the message to display in the user interface.
+     */
+    void showMessage(String text);
+
+    /**
+     * The method to display a list of cost item in the user interface.
+     *
+     * @param items the array of the cost items to display in the user interface.
+     * @param type  the string representing if the list of cost items is intended to be
+     *              displayed as the main table, as the table in the 'reports' frame or as a pie chart.
+     */
+    void showItems(CostItem[] items, String type);
+
+    /**
+     * The method to display the list of categories in the user interface.
+     *
+     * @param categories the array of the categories to display in the user interface.
+     */
+    void showCategories(Category[] categories);
 }
