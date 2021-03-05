45f1444e1db18878be616daf717db07c30899f53
halpert-keren <halpertkeren@gmail.com>
2021-03-05 18:25:19 +0200

    main CostManager code cleanup, optimization and comments

--- src/il/ac/shenkar/project/costmanager/CostManager.java
+++ src/il/ac/shenkar/project/costmanager/CostManager.java
@@ -1,20 +1,33 @@
 package il.ac.shenkar.project.costmanager;
 
-import il.ac.shenkar.project.costmanager.model.*;
-import il.ac.shenkar.project.costmanager.view.*;
-import il.ac.shenkar.project.costmanager.viewmodel.*;
+import il.ac.shenkar.project.costmanager.model.CostManagerException;
+import il.ac.shenkar.project.costmanager.model.DerbyDBModel;
+import il.ac.shenkar.project.costmanager.model.IModel;
+import il.ac.shenkar.project.costmanager.view.IView;
+import il.ac.shenkar.project.costmanager.view.View;
+import il.ac.shenkar.project.costmanager.viewmodel.IViewModel;
+import il.ac.shenkar.project.costmanager.viewmodel.ViewModel;
 
+/**
+ * The CostManager main class.
+ */
 public class CostManager {
+    /**
+     * Cost Manager main function. Runs the application.
+     */
     public static void main(String[] args) {
         try {
+            //creating the application components
             IModel model = new DerbyDBModel();
             IView view = new View();
             IViewModel vm = new ViewModel();
 
+            //connecting the components with each other
             view.setViewModel(vm);
             vm.setModel(model);
             vm.setView(view);
         } catch (CostManagerException e){
+            System.out.println(e.getMessage());
             e.printStackTrace();
         }
     }
--- src/il/ac/shenkar/project/costmanager/model/ModelUnitTest.java
+++ src/il/ac/shenkar/project/costmanager/model/ModelUnitTest.java
@@ -1,8 +1,5 @@
 package il.ac.shenkar.project.costmanager.model;
 
-import java.sql.Date;
-import java.util.Arrays;
-
 public class ModelUnitTest {
     public static void main(String[] args) {
         try {
@@ -13,7 +10,7 @@ public class ModelUnitTest {
 
 //            Category[] categories = model.getCategories();
 //            System.out.println(categories[1].getName());
-            CostItem costItem = new CostItem(2, "bb", 3.2, Currency.EURO, "2021-03-03", category);
+            CostItem costItem = new CostItem(2, "test", 3.2, Currency.EURO, "2021-03-03", category);
             model.addCostItem(costItem);
 
             CostItem[] costItems = model.getCostItems();
